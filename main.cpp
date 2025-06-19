1e12e468f568003f9c070f5cbf76ad0b3d1989c6
Fernando <imashfernando05@gmail.com>
2025-06-19 22:08:03 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,16 +1,68 @@
+
+// #include "Logger.h" da creare !!!
+#include <chrono>
+#include <iomanip>
+#include <sstream>
 #include <iostream>
-#include <fstream>
 #include <filesystem>
 
-using namespace std;
+namespace Logger
+{
 
-int main() {
-    ofstream MyFile("log_123.log");
+    std::ofstream Logger::logFile;
 
-    MyFile << "test log";
-    MyFile.close();
+    void Logger::Init()
+    // creare il file di log
+    {
+        // crea  la cartella logs se non esiste
+        // crea un file es: log_[date]_[version]_[debug].log
+    }
+
+    void Logger::SetConsoleOutput()
+    {
+        // attiva il console
+    }
+
+    void Logger::SetFileOutput()
+    {
+        // attiva il salvataggio dei log in un file es : log_[date]_[version]_[debug].log
+    }
+
+    void Logger::Write()
+    {
+        // scrivi nel file i log
+    }
+
+    std::string Logger::LevelToString(LogLevel level)
+    {
+        switch (level)
+        {
+        case LogLevel::Info:
+            return "INFO";
+        case LogLevel::Warning:
+            return "WARN";
+        case LogLevel::Error:
+            return "ERROR";
+        case LogLevel::Debug:
+            return "DEBUG";
+        default:
+            return "UNKNOWN";
+        }
+    }
     
-    cout << "File di log creato!" << endl;
-    return 0;
-    //testttt
+    //ora del log
+    std::string Logger::GetTimestamp()
+    {
+        auto now = std::chrono::system_clock::now();
+        auto time = std::chrono::system_clock::to_time_t(now);
+        std::stringstream ss;
+        ss << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S");
+        return ss.str();
+    }
+
+    void Logger::Log()
+    {
+        // log
+    }
+
 }
